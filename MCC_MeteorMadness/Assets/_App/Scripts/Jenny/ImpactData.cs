9932ae39046dff9664ba634fbfceb1c1fafbd5fe
DanielR <daniel.raducan05@gmail.com>
2025-10-04 16:20:17 +0100

    Deleted some scripts
    - ImpactData.cs
    - MeteorImpactParams

--- MCC_MeteorMadness/Assets/_App/Scripts/Jenny/ImpactData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ImpactData : MonoBehaviour
-{
-    // This data is to be set from the sliders/UI
-    [Header("Data to change")]
-    public float diameterMeters;   
-    public float speedKmPerSec;    
-}
