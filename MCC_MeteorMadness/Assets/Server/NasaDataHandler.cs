166662455716043c131e57cc06672178d8b2576b
Oladeji Sanyaolu <oladejisanyaolu@gmail.com>
2025-10-04 07:02:57 +0100

    continued api integration

---
+++ MCC_MeteorMadness/Assets/Server/NasaDataHandler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NasaDataHandler : MonoBehaviour
+{
+    // URL of the Flask server running on Python
+    private string apiUrl = "http://localhost:5000/get_neo_data";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start fetching NASA data via coroutine
+        StartCoroutine(GetNasaData());
+    }
+
+    // Coroutine to make HTTP request and parse the response
+    IEnumerator GetNasaData()
+    {
+        // Sending GET request to the Flask API
+        UnityWebRequest www = UnityWebRequest.Get(apiUrl);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            // Parse the JSON data from the server
+            string jsonResponse = www.downloadHandler.text;
+            Debug.Log("Received Data: " + jsonResponse);
+
+            // Process the NASA data
+            NasaData nasaData = ProcessNasaData(jsonResponse);
+            
+            // Example usage: Display information about asteroids
+            DisplayAsteroids(nasaData);
+        }
+        else
+        {
+            Debug.LogError("Error: " + www.error);
+        }
+    }
+
+    // Function to process the NASA data (deserialize JSON into usable objects)
+    NasaData ProcessNasaData(string jsonData)
+    {
+        NasaData data = JsonUtility.FromJson<NasaData>(jsonData);
+        return data;
+    }
+
+    // Example function to display asteroid details
+    void DisplayAsteroids(NasaData data)
+    {
+        foreach (var date in data.near_earth_objects)
+        {
+            Debug.Log($"Date: {date.Key}");
+            foreach (var asteroid in date.Value)
+            {
+                Debug.Log($"- Asteroid Magnitude: {asteroid.absolute_magnitude_h}");
+                foreach (var approach in asteroid.close_approach_data)
+                {
+                    Debug.Log($"  - Close Approach Date: {approach.close_approach_date}");
+                    Debug.Log($"  - Miss Distance (km): {approach.miss_distance.kilometers}");
+                    Debug.Log($"  - Relative Velocity (km/h): {approach.relative_velocity.kilometers_per_hour}");
+                }
+
+                Debug.Log($"- Estimated Diameter (max meters): {asteroid.estimated_diameter.meters.estimated_diameter_max}");
+                Debug.Log($"- Estimated Diameter (min meters): {asteroid.estimated_diameter.meters.estimated_diameter_min}");
+            }
+        }
+    }
+}
